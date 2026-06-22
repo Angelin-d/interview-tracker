74c61d6bb987ac140165d2d100eba6d254930255
Angelin Maria <angelinmariajose@gmail.com>
2026-06-22 12:39:22 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,11 +22,8 @@ app = FastAPI(
 # Configure CORS for React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "https://interview-tracker-uxsu.vercel.app"
-]
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
